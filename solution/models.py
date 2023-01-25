779f94ed8804e683ed89e7b2b600af4d3ce196cf
alonvais <alonv@mail.tau.ac.il>
2023-01-25 22:22:10 +0200

    get xception based model Doesn't seem to work at the moment

--- solution/models.py
+++ solution/models.py
@@ -34,6 +34,29 @@ class SimpleNet(nn.Module):
         return two_way_output
 
 
+class MLP_block(nn.Module):
+    def __init__(self):
+        super(MLP_block, self).__init__()
+
+        self.fc1 = nn.Linear(2048, 1000)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(1000, 256)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(256, 64)
+        self.relu3 = nn.ReLU()
+        self.fc4 = nn.Linear(64, 2)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        x = self.relu3(x)
+        x = self.fc4(x)
+        return x
+
+
 def get_xception_based_model() -> nn.Module:
     """Return an Xception-Based network.
 
@@ -41,5 +64,6 @@ def get_xception_based_model() -> nn.Module:
     (2) Override `custom_network`'s fc attribute with the binary
     classification head stated in the exercise.
     """
-    """INSERT YOUR CODE HERE, overrun return."""
-    return SimpleNet()
+    custom_network = build_xception_backbone(True)
+    custom_network.fc = MLP_block()
+    return custom_network
