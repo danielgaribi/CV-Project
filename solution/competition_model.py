ff05f31e09242a40ffcb90adc45a2475f96436ee
danielgaribi <danielgaribi@mail.tau.ac.il>
2023-01-29 00:24:40 +0200

    chapter6

--- solution/competition_model.py
+++ solution/competition_model.py
@@ -1,6 +1,6 @@
 """Define your architecture here."""
 import torch
-from models import SimpleNet
+from models import SimpleNet, get_xception_based_model
 
 
 def my_competition_model():
@@ -9,7 +9,7 @@ def my_competition_model():
     Do not change the model load line.
     """
     # initialize your model:
-    model = SimpleNet()
+    model = get_xception_based_model()
     # load your model using exactly this line (don't change it):
     model.load_state_dict(torch.load('checkpoints/competition_model.pt')['model'])
     return model
